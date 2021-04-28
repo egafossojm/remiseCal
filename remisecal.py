766de9a86b43865534d0aa00a4f24e84e9e32e66
Ega Fosso <egafosso@gmail.com>
2021-04-28 12:07:24 +0700

    Added Button fucntions and Exception management

--- remisecal.py
+++ remisecal.py
@@ -1,8 +1,9 @@
 from tkinter import Tk, Radiobutton, Label, Entry, Button, IntVar, StringVar
 
 
-class RemiseCal:
 
+
+class RemiseCal:
     def __init__(self):
         window = Tk()
         window.title("Calcul de remise")
@@ -39,21 +40,41 @@ class RemiseCal:
 
         discount_amount_lbl = Label(window, text="Valeur Remise", fg="black")
         discount_amount_lbl.grid(column=1, row=2, padx=40)
-        discount_amount_entry = Entry(window, textvariable=self.final_cost, width=14)
+        discount_amount_entry = Entry(window, textvariable=self.discount, width=14)
         discount_amount_entry.grid(column=2, row=2)
 
         final_cost_lbl = Label(window, text="Montant final", fg="black")
         final_cost_lbl.grid(column=1, row=3, padx=40)
-        final_cost_entry = Entry(window, textvariable=self.discount, fg="green", width=14)
+        final_cost_entry = Entry(window, textvariable=self.final_cost, fg="green", width=14)
         final_cost_entry.grid(column=2, row=3)
+        
+        global error_lbl
+        error_lbl = Label(window, text="Valeur Incorrecte ! Nombre reel attendu", fg="red")
 
-        calculate_btn = Button(window, text="Calculer", bg="light green", fg="black", width=14, command="")
+        calculate_btn = Button(window, text="Calculer", bg="light green", fg="black", width=14, command=self.calculate)
         calculate_btn.grid(column=1, row=6)
 
-        clear_btn = Button(window, text="Effacer", bg="light gray", fg="black", width=14, command="")
+        clear_btn = Button(window, text="Effacer", bg="light gray", fg="black", width=14, command=self.clear)
         clear_btn.grid(column=2, row=6)
 
-
         window.mainloop()
+
+    def calculate(self):
+        error_lbl.grid_remove()
+        self.final_cost.set("")
+        self.discount.set("")
+        try:
+            percentage = float(self.bill_cost.get()) * self.discount_percent.get() / 100
+            self.discount.set(percentage)
+            self.final_cost.set(float(self.bill_cost.get()) - percentage)
+        except ValueError:
+            error_lbl.grid(column=1, row=1, columnspan=2)
+
+    def clear(self):
+        self.final_cost.set("")
+        self.discount.set("")
+        self.bill_cost.set("")
+        error_lbl.grid_remove()
+
 
 RemiseCal()
